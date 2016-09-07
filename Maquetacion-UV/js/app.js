d7911da4aa9090c4d830d079263081aa004e404b
josecaos <jazzvoon@gmail.com>
2016-09-07 03:52:02 -0500

    Fixes al home roto + ajustes a dependencias + agrego 'fontawesome' y slick-slider + fixes al menu y footer

--- Maquetacion-UV/js/app.js
+++ Maquetacion-UV/js/app.js
@@ -5,18 +5,19 @@ var u;
 
 $(document).ready(function() {
 
-u = new Utils();
+  u = new Utils();
 
-u.vcenter();
+  u.vcenter();
 
-$('.imgLiquid.imgLiquidFill').imgLiquid();
-$('.imgLiquid.imgLiquidNoFill').imgLiquid({fill:false});
+  $('.imgLiquid.imgLiquidFill').imgLiquid();
+  $('.imgLiquid.imgLiquidNoFill').imgLiquid({fill:false});
 
 
-//reajuste para cambio de pantalla
-u.addWindowResizeFunction(
-     function() {
-         u.vcenter();
+  //reajuste para cambio de pantalla
+  u.addWindowResizeFunction(
+    function() {
+      u.vcenter();
 
-      }
-   );
+    });
+
+  });
