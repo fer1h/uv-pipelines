c93b720842f04199817a029180d54a66cda952e9
MacBook <macbook@MacBook-Pro-de-Fer.local>
2016-09-10 19:55:29 -0500

    header y nosotros

--- nosotros.php
+++ nosotros.php
@@ -2,9 +2,32 @@
 
 include_once "secciones/compartidas/header.php";
 
+ ?>
 
+<!-- Título ---------------------------------------->
 
+ <div id="título" class="columns h-20 p-0">
+   <div class="columns h-100 imgLiquid imgLiquidFill">
+     <img src="./img/titulo-servicios.png" alt=""/>
+     <div class="columns h-70 mt1 mb1  text-sm-center">
+       <p class="titulo-master">Nosotros</p> <p class="titulo-master-2">Conoce más acerca de nuestra institución</p>
+     </div>
+   </div>
+ </div>
 
+ <!-- termina título ---------------------------------------->
+<div id="servicioContenido " class="columns p-2">
+ <h3 class="font-lg-L text-sm-center">UV PIPELINES</h3>
+ </br>
+ <div class="columns h-a color-rojo-bd small-4 medium-4 large-12 text-sm-center"></div>
+ </br>
+ </br>
+<p class="text-sm-center">
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ante tellus, efficitur non arcu ullamcorper, fringilla cursus quam. Cras in mattis nisl, sollicitudin placerat purus. Mauris iaculis turpis in ipsum mattis placerat. Cras sit amet venenatis quam. Mauris nisl elit, pellentesque a commodo tincidunt, dapibus vel nunc. Praesent porttitor, ex vitae bibendum mollis, enim risus lobortis odio, nec varius ex mi at diam. Sed ut rhoncus lacus, in facilisis felis. Nulla tincidunt leo in erat mollis mollis. Donec ultricies quam non </br>
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ante tellus, efficitur non arcu ullamcorper, fringilla cursus quam. Cras in mattis nisl, sollicitudin placerat purus. Mauris iaculis turpis in ipsum mattis placerat. Cras sit amet venenatis quam. Mauris nisl elit, pellentesque a commodo tincidunt, dapibus vel nunc. Praesent porttitor, ex vitae bibendum mollis, enim risus lobortis odio, nec varius ex mi at diam. Sed ut rhoncus lacus, in facilisis felis. Nulla tincidunt leo in erat mollis mollis. Donec ultricies quam non</p>
+</div>
+
+<?php
 
 include_once "secciones/compartidas/footer.php";
 
