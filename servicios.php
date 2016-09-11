19cdd06ec58face725c48f8947ef9b63ed3f7415
josecaos <jazzvoon@gmail.com>
2016-09-10 20:53:33 -0500

    fixes bugs template servicios

--- servicios.php
+++ servicios.php
@@ -2,116 +2,109 @@
 
 include_once "secciones/compartidas/header.php";
 
- ?>
-    <!-- Services -->
+?>
+<!-- Services -->
 
-      <!-- título ---------------------------------------->
+<!-- título ---------------------------------------->
 
-      <div id="título" class="columns h-20 p-0">
-        <div class="columns h-100 imgLiquid imgLiquidFill">
-          <img src="./img/titulo-servicios.png" alt=""/>
-          <div class="columns h-70 mt1 mb1  text-sm-center">
-            <p class="titulo-master">Ingeniería & Certificación</p> <p class="titulo-master-2">ELABORACIÓN, CERTIFICACIÓN DE INGENIERÍA & OWNER'S ENGINEERING</p>
-          </div>
-        </div>
-      </div>
-
-        <!-- termina título ---------------------------------------->
-
-        <div id="main-services" class="columns p-1">
-        <!-- Menú lateral -->
-
-      <div id="infoLateral" class="columns large-4">
-        <div id="navLateral" class="columns p-1">
-
-          <h3 class="font-lg-L">CATEGORÍAS</h3>
-
-<div class="columns h-a color-rojo-bd small-4 medium-4 large-4"></div>
-
-</br>
-
-          <ul class="menu vertical">
-            <li><a href="#" class="red-link-c">Ingeniería & Certificación</a>
-              <ul class="nested vertical menu">
-                <li><a href="#" class="black-link">Elaboración, certificación de ingeniería & owner's engineering</a></li>
-                <li><a href="#" class="black-link">Unidades de verificación</a></li>
-                <li><a href="#" class="black-link">Certificación de seguridad técnica</a></li>
-                <li><a href="#" class="black-link">Inspección - expeditación, supervisión/PMC</a></li>
-                <li><a href="#" class="black-link">Consultoría & entrenamiento</a></li>
-              </ul>
-            </li>
-            <li><a href="#" class="black-link-c">Construcción</a>
-              <ul class="nested vertical menu">
-                <li><a href="#" class="black-link">Diseño & fabricación</a></li>
-                <li><a href="#" class="black-link">Inspección & pruebas</a></li>
-                <li><a href="#" class="black-link">Soluciones para el manejo y transporte de gas natural</a></li>
-              </ul>
-            </li>
-            <li><a href="#" class="black-link-c">Instrumentación & control</a>
-              <ul class="nested vertical menu">
-                <li><a href="#" class="black-link">Computadores y medidores de flujo  para gas natural, combustibles refinados, agua potable y agua ratada</a></li>
-                <li><a href="#" class="black-link">Sistemas de control de carga de combustibles refinados</a></li>
-                <li><a href="#" class="black-link">Medidores de flujo tipo ultrasónicos no intrusivos</a></li>
-                <li><a href="#" class="black-link">Sistemas de medición de flujo en canales abiertos</a></li>
-                <li><a href="#" class="black-link">Servicios de verificación y aforo</a></li>
-                <li><a href="#" class="black-link">Objetivos del servicio de verificación y aforo</a></li>
-                <li><a href="#" class="black-link">Equipos utilizados en la verificación de caudal</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-
-      </div>
-
-        <!-- Termina Menú lateral -->
-
-        <!-- Info servicioContenido -->
-
-        <div id="servicioContenido " class="columns large-8">
-
-          <div class="columns p-1 h-65-v imgLiquid imgLiquidFill">
-            <img src="./img/1.png" alt="" />
-          </div>
-          <div class="columns p-1 h-40-v">
-            <h3 class="font-lg-XL">Elaboración, certificación de ingeniería & owner's engineering</h3>
-
-          </br>
-          <p>  Ingeniería básica y de detalle considerando las siguientes disciplinas:</p>
-          <ul>
-            <li>Ingeniería Civil - Estructural</li>
-            <li>Mecánica (tubería y soldadura)</li>
-            <li>Ingeniería eléctrica</li>
-            <li>Proceso</li>
-            <li>Instrumentación y Control</li>
-            <li>Ingeniería ambiental (Análisis de Riesgo)</li>
-          </ul>
-        </br>
-        <p>Desarrollo de ingeniería para las siguientes instalaciones:</p>
-        <ul>
-          <li>Ductos Terrestres y Marinos</li>
-          <li>Plantas de Proceso</li>
-          <li>Tanques de Almacenamiento</li>
-          <li>Estaciones de Compresión y Bombeo</li>
-          <li>Estaciones de Regulación, Medición y Control</li>
-        </ul>
-      </div>
-
-      <div  class="columns"></div>
-
+<div id="título" class="columns h-20 p-0">
+  <div class="columns h-100 imgLiquid imgLiquidFill">
+    <img src="./img/titulo-servicios.png" alt=""/>
+    <div class="columns h-70 mt1 mb1  text-sm-center">
+      <p class="titulo-master">Ingeniería & Certificación</p> <p class="titulo-master-2">ELABORACIÓN, CERTIFICACIÓN DE INGENIERÍA & OWNER'S ENGINEERING</p>
     </div>
+  </div>
+</div>
 
+<!-- termina título ---------------------------------------->
 
+<div id="main-services" class="columns h-a p-1">
+  <!-- Menú lateral -->
 
+  <div id="infoLateral" class="columns h-a large-4">
+    <div id="navLateral" class="columns p-1">
+
+      <h3 class="font-lg-L">CATEGORÍAS</h3>
+
+      <div class="columns h-a color-rojo-bd small-4 medium-4 large-4"></div>
+
+    </br>
+
+    <ul class="menu vertical">
+      <li>Ingeniería & Certificación
+        <ul class="nested vertical menu">
+          <li><a href="#" class="black-link">Elaboración, certificación de ingeniería & owner's engineering</a></li>
+          <li><a href="#" class="black-link">Unidades de verificación</a></li>
+          <li><a href="#" class="black-link">Certificación de seguridad técnica</a></li>
+          <li><a href="#" class="black-link">Inspección - expeditación, supervisión/PMC</a></li>
+          <li><a href="#" class="black-link">Consultoría & entrenamiento</a></li>
+        </ul>
+      </li>
+      <li><a href="#" class="black-link-c">Construcción</a>
+        <ul class="nested vertical menu">
+          <li><a href="#" class="black-link">Diseño & fabricación</a></li>
+          <li><a href="#" class="black-link">Inspección & pruebas</a></li>
+          <li><a href="#" class="black-link">Soluciones para el manejo y transporte de gas natural</a></li>
+        </ul>
+      </li>
+      <li><a href="#" class="black-link-c">Instrumentación & control</a>
+        <ul class="nested vertical menu">
+          <li><a href="#" class="black-link">Computadores y medidores de flujo  para gas natural, combustibles refinados, agua potable y agua ratada</a></li>
+          <li><a href="#" class="black-link">Sistemas de control de carga de combustibles refinados</a></li>
+          <li><a href="#" class="black-link">Medidores de flujo tipo ultrasónicos no intrusivos</a></li>
+          <li><a href="#" class="black-link">Sistemas de medición de flujo en canales abiertos</a></li>
+          <li><a href="#" class="black-link">Servicios de verificación y aforo</a></li>
+          <li><a href="#" class="black-link">Objetivos del servicio de verificación y aforo</a></li>
+          <li><a href="#" class="black-link">Equipos utilizados en la verificación de caudal</a></li>
+        </ul>
+      </li>
+    </ul>
   </div>
 
-  <!-- Termina servicios -->
+</div>
 
-    </div>
+<!-- Termina Menú lateral -->
 
-    <!-- Termina servicios -->
+<!-- Info servicioContenido -->
+
+<div id="servicioContenido " class="columns h-a large-8">
+
+  <div class="columns p-1 h-65-v imgLiquid imgLiquidFill">
+    <img src="./img/1.png" alt="" />
+  </div>
+  <div class="columns p-1">
+    <h3 class="font-lg-XL">Elaboración, certificación de ingeniería & owner's engineering</h3>
+
+  </br>
+  <p>  Ingeniería básica y de detalle considerando las siguientes disciplinas:</p>
+  <ul>
+    <li>Ingeniería Civil - Estructural</li>
+    <li>Mecánica (tubería y soldadura)</li>
+    <li>Ingeniería eléctrica</li>
+    <li>Proceso</li>
+    <li>Instrumentación y Control</li>
+    <li>Ingeniería ambiental (Análisis de Riesgo)</li>
+  </ul>
+</br>
+<p>Desarrollo de ingeniería para las siguientes instalaciones:</p>
+<ul>
+  <li>Ductos Terrestres y Marinos</li>
+  <li>Plantas de Proceso</li>
+  <li>Tanques de Almacenamiento</li>
+  <li>Estaciones de Compresión y Bombeo</li>
+  <li>Estaciones de Regulación, Medición y Control</li>
+</ul>
+</div>
+</div>
+
+
+
+</div>
+
+<!-- Termina servicios -->
 
 <?php
 
 include_once "secciones/compartidas/footer.php";
 
- ?>
+?>
