3091e48ae5016838cf5b83028956e1bcad909e6e
fer1h <fer@dothemath.mx>
2016-09-14 21:51:04 -0500

    square

--- secciones/compartidas/header.php
+++ secciones/compartidas/header.php
@@ -76,6 +76,7 @@
 
       <div id="img-hora" class="columns large-4 right h-100 p-0">
         <div class="box-red"></div>
+        <div class="columns">Lorem </br> Ipsum</div>
       </div>
 
     </div>
